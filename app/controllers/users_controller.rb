9a407c3b0f0b1e933297b76894ea9a9154f5a6f3
koo-ke <hope14kou@hotmail.co.jp>
2019-06-05 16:36:26 +0900

    ビュー画面へのルーテングとコントローラを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def index
+  end
+  
+  def show
+  end
+
+  def signup
+  end
+
+  def paying
+  end
+
+  def information
+  end
+
+  def login
+  end
+
+  def logout
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'products#index'
+  
+  resources :users do
+
+    collection do
+      get 'signup'
+      get 'paying'
+      get 'information'
+      get 'login'
+      get 'logout'
+    end
+  end
+
+  resources :items do
+
+    member do
+      get 'purchasing'
+    end
+  end
+
 end
