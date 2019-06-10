b651a21b99bd196cc3203b80cd6315aa9a79bfcb
向川　英孝 <jinsino1987@gmail.com>
2019-06-10 19:37:22 +0900

    Update breadcrumbs.rb

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,38 +7,23 @@ crumb :mypage do
   parent :root
 end
 
-crumb :card do
-  link "支払い方法", card_users_path
+crumb :paying do
+  link "支払い方法", paying_users_path
   parent :mypage
 end
 
-crumb :profile do
-  link "プロフィール", profile_users_path
+crumb :show do
+  link "プロフィール", user_path
   parent :mypage
 end
 
-crumb :identification do
-  link "本人情報の登録", identification_users_path
+crumb :information do
+  link "本人情報の登録", information_users_path
   parent :mypage
 end
 
 crumb :logout do
   link "ログアウト", logout_users_path
-  parent :mypage
-end
-
-crumb :exhibition do
-  link "出品した商品-出品中", exhibition_users_path
-  parent :mypage
-end
-
-crumb :trading do
-  link "出品した商品-取引中", trading_users_path
-  parent :mypage
-end
-
-crumb :sold do
-  link "出品した商品-売却済み", sold_users_path
   parent :mypage
 end
 
