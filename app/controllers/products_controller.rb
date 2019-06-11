7890816e517bdcc7b92dd7d8782c1f16908e16ee
hrm-hbcm <hbcmasdf@gmail.com>
2019-06-11 21:50:30 +0900

    ルーティング、コントローラー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,7 @@ class ProductsController < ApplicationController
 
   def create
     Product.create!(product_params)
-    redirect_to action: index
+    redirect_to root_path
   end
 
   def purchasing
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   root 'products#index'
   
-  resources :products, only: [:new, :create, :show] do
+  resources :products do
     member do
       get 'purchasing'
     end
