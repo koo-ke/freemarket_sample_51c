dd68670d85a9e57d0eb69a5c6a658cae701d7ba9
向川　英孝 <jinsino1987@gmail.com>
2019-06-11 15:23:24 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,10 @@ class ProductsController < ApplicationController
     Product.create!(product_params)
   end
 
+  def edit
+    
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :price, :publish_status, :text, :size, :shipping_charges, :shipping_origin_area, :days_to_ship, :buyer_id, :item_status, images: []).merge(saler_id: 1)
