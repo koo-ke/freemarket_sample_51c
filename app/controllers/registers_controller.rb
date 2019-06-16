d65f8908a423535abd73e31929bef4b10a2c7a53
Takabun <takafumi.kido@gmail.com>
2019-06-16 10:54:29 +0900

    住所登録が出来るようになった

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -7,12 +7,24 @@ class RegistersController < ApplicationController
  end
 
  def memberaddfress
- end
  
+ end
+
+ def create
+  Address.create(address_params)
+  redirect_to payment_registers_path
+ end
+
+
  def payment
  end
 
  def completion
  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :prefecture, :city, :address,:building).merge(user_id: current_user.id)
+  end
 
 end
