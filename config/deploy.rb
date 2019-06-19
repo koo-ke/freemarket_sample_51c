2aebbcac22a896ad50af47c74230491692f5888a
hrm-hbcm <hbcmasdf@gmail.com>
2019-06-19 10:55:21 +0900

    payjp環境変数(濱田)

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ set :default_env, {
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+  PAYJP_PRIVATE_KEY: ENV["PAYJP_PRIVATE_KEY"]
+  PAYJP_KEY: ENV["PAYJP_KEY"]
 }
 
 set :linked_files, %w{ config/application.yml }
