877241b94ef5dafbcc305afbd42c94f065e48611
koo-ke <hope14kou@hotmail.co.jp>
2019-06-19 11:58:42 +0900

    merge(saler_id: 1) → merge(saler_id: current_user.id)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -102,7 +102,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :price, :buyer_id, :publish_status, :text, :size, :shipping_charges, :shipping_origin_area, :days_to_ship, :item_status, :category_ids, images: []).merge(saler_id: 1)
+    params.require(:product).permit(:name, :price, :buyer_id, :publish_status, :text, :size, :shipping_charges, :shipping_origin_area, :days_to_ship, :item_status, :category_ids, images: []).merge(saler_id: current_user.id)
   end
 
 
